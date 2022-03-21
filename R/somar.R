15b550eb3fe72ee0d6772bff4230da82350882b8
samuel <samucadeus23@gmail.com>
2022-03-21 18:04:36 -0300

    Alterado

---
+++ R/somar.R
@@ -0,0 +1,12 @@
+#'Função para somar
+#'
+#'uma função que soma dois números
+#'@param a número inteiro
+#'@param B número inteiro
+#'
+#'@examples
+#'somar(1,1)
+#'@export
+somar <- function(a, b){
+  a + b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,12 @@
+#'Função para subtrair
+#'
+#'uma função que subtrai dois números
+#'@param a número inteiro
+#'@param b número inteiro
+#'
+#'@examples
+#'subtrair(1,1)
+#'@export
+subtrair <- function(a, b){
+  a - b
+}
